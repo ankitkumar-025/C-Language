c0c58622e2f32c400ec804b63df9be0814f5dd0d
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-13 21:24:42 +0530

    Add files via upload

---
+++ Areaofsquare.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float side;
+    printf("Enter side :");
+    scanf("%f",&side);
+    float area;
+    area=side*side;
+    printf("The area of the sqaure is %f",area);
+    return 0;
+    
+}
