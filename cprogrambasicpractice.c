7e2f08c7716e7c0420b644ba056b7848b97c340a
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-14 21:59:14 +0530

    Add files via upload

---
+++ cprogrambasicpractice.c
@@ -0,0 +1,13 @@
+//C PROGRAM TO TAKE AN INTEGER AS INPUT AND PRINT IT
+#include<stdio.h>
+int main()
+
+{
+   int x;
+   printf("enter a integer");
+   scanf("%d",&x);
+
+   printf("entered integer is:%d",x);
+   return 0;
+
+}
