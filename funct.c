7b5663cc9e60d6b6209ae6c08f6b0db85a09b2f5
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-17 23:45:07 +0530

    Add files via upload

---
+++ funct.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int square(int num);
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    square(num);
+
+
+}
+int square(int num)
+{
+    int result=num*num;
+    printf("Square of the given no is: %d",result);
+    }
