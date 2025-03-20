e9b8c5a75d0cdd479d4282e33b4a957d8060c801
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-20 19:33:38 +0530

    Add files via upload

---
+++ Loops.c
@@ -0,0 +1,13 @@
+//By User... 
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number:");
+    scanf("%d",&n);
+
+    for(int i=0;i<=n;i++)
+    {
+        printf("%d ",i);
+    }
+}
