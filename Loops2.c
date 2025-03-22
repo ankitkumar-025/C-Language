64265bc0eec0f0bca1fb4dd915c1b12577f4e2c0
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-22 20:13:25 +0530

    Add files via upload

---
+++ Loops2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("The sum of the no is %d\n",sum);
+
+for(int i=n;i>=1;i--)
+{
+    printf("%d\n",i);
+}
+}
