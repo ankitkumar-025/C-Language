27008ccf31dd66e69bc6b09e793424ce30e3d063
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-29 19:20:52 +0530

    Add files via upload

---
+++ VolumeOfSphere.c
@@ -0,0 +1,16 @@
+//VOLUME OF SPHERE//
+#include<stdio.h>
+int main()
+{
+    int r=3.5;
+    float v =4*3.14*r*r*r/3;
+    printf("VOLUME OF SPHERE IS: %f",v);
+
+ //AREA OF CIRCLE//
+  float a=3.14*r*r;
+ printf("\nAREA OF CIRCLE IS:%f",a);
+
+
+
+
+}
