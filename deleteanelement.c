9eb87a04e2336b49793b517813cd642e1945f0ac
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-12 19:51:04 +0530

    Update deleteanelement.c

--- deleteanelement.c
+++ deleteanelement.c
@@ -4,7 +4,7 @@ void main()
 {
     int arr1[50], i, pos, n;
     printf("Input the size of array : ");
-    scanf("%d", &n);
+    scanf("%d", &n);h
     /* Stored values into the array*/
     printf("Input %d elements in the array in ascending order:\n", n);
     for (i = 0; i < n; i++)
