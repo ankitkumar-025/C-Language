2deaaacbc29a5180255b91d1569a56d8dbc48762
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-22 01:46:23 +0530

    Update Incrementoperator.c

--- Incrementoperator.c
+++ Incrementoperator.c
@@ -2,7 +2,7 @@
 int main()
 {
     //increment operator
-    //++i(pre increment)  i++(post increment)
+    //++i(pre increment)  i++(post increment)vv
      int i=1;
      printf("%d\n",i++);
      printf("%d",i);
