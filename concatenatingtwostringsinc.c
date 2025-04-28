8b9b0cec5911bd9fb4da7ba3beaf5c521b65f8b3
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-28 22:41:17 +0530

    Update concatenatingtwostringsinc.c

--- concatenatingtwostringsinc.c
+++ concatenatingtwostringsinc.c
@@ -8,7 +8,7 @@ int main()
     //declare a new strings to store the concatenated string
 
     char str3[100];
-    int i=0;ssssss
+    int i=0;ssssssaaaa
     int j=0;
     printf("\n first string is: %s",str1);
     printf("\nsecond string is: %s",str2);
